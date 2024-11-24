38d8f1f79c00238e4f8d7bc3014d0cd10dfe1d3b
m0rfey <plaksinaleks2306@mail.ru>
2024-11-24 21:12:33 +0500

    Файлы в операционной системе

---
+++ module_7_5.py
@@ -0,0 +1,28 @@
+# Файлы в операционной системе
+import os
+import time
+
+# Укажите директорию, которую нужно обойти
+directory = "."
+
+# Обход каталога
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        # Формируем полный путь к файлу
+        filepath = os.path.join(root, file)
+
+        # Получаем время последнего изменения файла
+        filetime = os.path.getmtime(filepath)
+
+        # Форматируем время в читаемом формате
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        # Получаем размер файла
+        filesize = os.path.getsize(filepath)
+
+        # Получаем родительскую директорию файла
+        parent_dir = os.path.dirname(filepath)
+
+        # Выводим информацию о файле
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
